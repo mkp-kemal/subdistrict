2c3cb76719eb2f0bc740ca35c1fc41180f6deeef
mkp-kemal <kemal.garut7@gmail.com>
2024-07-30 08:18:53 +0700

    --

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -270,29 +270,7 @@ const Dashboard = () => {
                     <ImSpinner10 className="text-4xl animate-spin text-tosca" />
                 </div>
             ) : (
-                <div className="p-4">
-                    <Row gutter={16} className="mb-6 p-4 rounded-lg">
-                        <Col span={12}>
-                            <Card title={<span className='text-white'>Jumlah Blog</span>} bordered={false} className="p-4 text-white font-bold bg-tosca shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                                {blogCount}
-                            </Card>
-                        </Col>
-                        <Col span={12}>
-                            <Card title={<span className='text-white'>Jumlah Admin</span>} bordered={false} className="p-4 bg-lime-900 text-white font-bold shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                                {adminCount}
-                            </Card>
-                        </Col>
-                    </Row>
-                    <div className="mt-6">
-                        <Table
-                            columns={columns}
-                            dataSource={data}
-                            scroll={{ x: '100%' }}
-                        />
-                    </div>
-                  
-
-                </div>
+                
             )}
         </>
     );
