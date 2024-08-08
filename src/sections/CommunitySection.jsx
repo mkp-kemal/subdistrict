fa6bedfb7bb806d2801a0068c865d615f7fd2000
mkp-kemal <kemal.garut7@gmail.com>
2024-08-08 09:59:38 +0700

    add bumdes

--- src/sections/CommunitySection.jsx
+++ src/sections/CommunitySection.jsx
@@ -1,17 +1,36 @@
+import { Tabs } from 'antd';
+import BumdesContent from './content/BumdesContent';
+
+const { TabPane } = Tabs;
+
 const CommunitySection = () => {
     return (
-        <div className="bg-gray-300 p-2 mt-36">
+        <div className="bg-white p-2 mt-36">
             <div className="container mx-auto px-4">
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="text-center">
-                            <h2 className="text-3xl font-bold">Community</h2>
+                            <h2 className="text-3xl font-bold mb-4">Community</h2>
                         </div>
                     </div>
                 </div>
+                <Tabs defaultActiveKey="1" centered>
+                    <TabPane tab="BumDes" key="1">
+                       <BumdesContent />
+                    </TabPane>
+                    <TabPane tab="Linmas" key="2">
+                        <p>Content for Linmas</p>
+                    </TabPane>
+                    <TabPane tab="DKM" key="3">
+                        <p>Content for DKM</p>
+                    </TabPane>
+                    <TabPane tab="MUI" key="4">
+                        <p>Content for MUI</p>
+                    </TabPane>
+                </Tabs>
             </div>
         </div>
-    )
+    );
 }
 
-export default CommunitySection
+export default CommunitySection;
