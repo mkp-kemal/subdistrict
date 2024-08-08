252224ceebd9e7faf5ce388b1fa753c555d7b25e
HilmiAliSalsabila <alihilmi93@gmail.com>
2024-08-08 16:27:13 +0700

    add detail curug cipondok and dayang sumbi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import NavbarComponent from './components/NavbarComponent';
-import {HomeSection} from './sections/HomeSection';
+import { HomeSection } from './sections/HomeSection';
 import ProfileSection from './sections/ProfileSection';
 import ActivityKknSection from './sections/ActivityKknSection';
 import { AdminSection } from './sections/AdminSection';
@@ -11,6 +11,8 @@ import NotFound from './components/NotFound';
 import ApbdSection from './sections/ApbdSection';
 import JdihSection from './sections/JdihSection';
 import AttractionDetailKoleangkak from './sections/AttractionDetail';
+import AttractionDetailCipondok from './sections/CipondokDetail';
+import AttractionDetailDayangSumbi from './sections/DayangSumbiDetail';
 import CommunitySection from './sections/CommunitySection';
 
 const App = () => {
@@ -38,6 +40,8 @@ const MainRoutes = () => {
           <Route path="/community" element={<CommunitySection />} />
           <Route path="/jdih" element={<JdihSection />} />
           <Route path="/wisata/curug-koleangkak" element={<AttractionDetailKoleangkak />} />
+          <Route path="/wisata/curug-cipondok" element={<AttractionDetailCipondok />} />
+          <Route path="/wisata/curug-dayang-sumbi" element={<AttractionDetailDayangSumbi />} />
           <Route path="/blog/:id" element={<BlogDetailSection />} />
           <Route path="/user/admin" element={<AdminSection />} />
           <Route path="/auth/login" element={<Login />} />
