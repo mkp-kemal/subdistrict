cbc817b9f637c6d6a3bb58ecbd698098c8f52fdd
mkp-kemal <kemal.garut7@gmail.com>
2024-08-08 09:42:33 +0700

    add community

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import NavbarComponent from './components/NavbarComponent';
-import { HomeSection } from './sections/HomeSection';
+import {HomeSection} from './sections/HomeSection';
 import ProfileSection from './sections/ProfileSection';
 import ActivityKknSection from './sections/ActivityKknSection';
 import { AdminSection } from './sections/AdminSection';
@@ -11,8 +11,7 @@ import NotFound from './components/NotFound';
 import ApbdSection from './sections/ApbdSection';
 import JdihSection from './sections/JdihSection';
 import AttractionDetailKoleangkak from './sections/AttractionDetail';
-import AttractionDetailCipondok from './sections/CipondokDetail';
-import AttractionDetailDayangSumbi from './sections/DayangSumbiDetail';
+import CommunitySection from './sections/CommunitySection';
 
 const App = () => {
   return (
@@ -36,10 +35,9 @@ const MainRoutes = () => {
           <Route path="/profile" element={<ProfileSection />} />
           <Route path="/activity" element={<ActivityKknSection />} />
           <Route path="/apbd" element={<ApbdSection />} />
+          <Route path="/community" element={<CommunitySection />} />
           <Route path="/jdih" element={<JdihSection />} />
           <Route path="/wisata/curug-koleangkak" element={<AttractionDetailKoleangkak />} />
-          <Route path="/wisata/curug-cipondok" element={<AttractionDetailCipondok />} />
-          <Route path="/wisata/curug-dayang-sumbi" element={<AttractionDetailDayangSumbi />} />
           <Route path="/blog/:id" element={<BlogDetailSection />} />
           <Route path="/user/admin" element={<AdminSection />} />
           <Route path="/auth/login" element={<Login />} />
---
+++ src/sections/CommunitySection.jsx
@@ -0,0 +1,17 @@
+const CommunitySection = () => {
+    return (
+        <div className="bg-gray-300 p-2 mt-36">
+            <div className="container mx-auto px-4">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="text-center">
+                            <h2 className="text-3xl font-bold">Community</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CommunitySection
