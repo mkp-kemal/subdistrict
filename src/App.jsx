e9952178906ce65c0ccf6fdad5c4d3ea297d0c29
mkp-kemal <kemal.garut7@gmail.com>
2024-08-13 11:18:36 +0700

    update attractions

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import JdihSection from './sections/JdihSection';
 import AttractionDetailKoleangkak from './sections/AttractionDetail';
 import CommunitySection from './sections/CommunitySection';
 import AttractionDetailCipondok from './sections/CipondokDetail';
+import AttractionDetailDayangSumbi from './sections/DayangSumbiDetail';
 
 const App = () => {
   return (
@@ -40,6 +41,7 @@ const MainRoutes = () => {
           <Route path="/jdih" element={<JdihSection />} />
           <Route path="/wisata/curug-koleangkak" element={<AttractionDetailKoleangkak />} />
           <Route path="/wisata/curug-cipondok" element={<AttractionDetailCipondok />} />
+          <Route path="/wisata/curug-dayang-sumbi" element={<AttractionDetailDayangSumbi />} />
           <Route path="/blog/:id" element={<BlogDetailSection />} />
           <Route path="/user/admin" element={<AdminSection />} />
           <Route path="/auth/login" element={<Login />} />
